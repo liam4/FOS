fab780d13dbe882e2c576215c28b2645f6314175
DatOneLefty <k9dogs101@gmail.com>
2015-10-10 13:33:30 -0400

    Build 39 - 40
    build 39 -30 official

--- Term/commands/catfish.py
+++ Term/commands/catfish.py
@@ -1,3 +1,3 @@
 print "Pictus Catfish are awesome!"
-print "This is OS is powered by Pictus Catfish... because the are fast"
+print "This is OS is powered by Pictus Catfish... because the are awesome"
 
---
+++ Term/commands/credits.py
@@ -0,0 +1,7 @@
+print "CREDITS"
+print "DEVELOPERS"
+print "DATONELEFTY - MAIN DEVELOPER"
+print "CloudFlare - Web protection
+print ""
+print "BETA TESTERS"
+print "DatOneLefty - Main Tester"
